0221e2d8e1cb1814a14a67c0be638b4a16de72a4
Kyle Berry <kyleberry@Kyles-MacBook-Pro.local>
2019-01-14 20:54:32 -0700

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/'
+// export const key = '2c8ad358947a3ad18cceb055c6cc4263'
+export const key = '2c8ad358947a3ad18cceb055c6cc4263'
+// ff488d24342fc4aa3c41e95436245f21
